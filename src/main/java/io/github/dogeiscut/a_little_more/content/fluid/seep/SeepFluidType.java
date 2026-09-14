e49eda7019c73155d62de09eb105ae888f20f089
DogeisCut <23178844+DogeisCut@users.noreply.github.com>
2026-09-14 04:04:47 -0600

    aparently i had accidently removed this too

--- src/main/java/io/github/dogeiscut/a_little_more/content/fluid/seep/SeepFluidType.java
+++ src/main/java/io/github/dogeiscut/a_little_more/content/fluid/seep/SeepFluidType.java
@@ -21,6 +21,7 @@ public class SeepFluidType extends BaseFluidType {
                         .fallDistanceModifier(0f)
                         .canExtinguish(true)
                         .canDrown(false)
+                        .lightLevel(2)
                         .sound(SoundActions.BUCKET_FILL, SoundEvents.BUCKET_FILL)
                         .sound(SoundActions.BUCKET_EMPTY, SoundEvents.BUCKET_EMPTY)
                         .sound(SoundActions.FLUID_VAPORIZE, SoundEvents.FIRE_EXTINGUISH)
